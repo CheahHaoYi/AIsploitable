03bebe3d4ffedc195fc861672c7d87ab346ad3ea
CheahHaoYi <haoyicheah@gmail.com>
2026-08-23 22:53:07 +0800

    fix path

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const isStaticExport = process.env.NEXT_EXPORT === 'true' || process.env.NODE_ENV === 'production';
 
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
+  outputFileTracingRoot: path.join(__dirname, './'),
   images: {
     unoptimized: true,
   },
